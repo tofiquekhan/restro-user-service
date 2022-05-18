9f6fc17b5e7b654a1895adab492bc267210595d6
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-05-18 18:11:47 +0530

    Initial Commit

---
+++ src/main/java/com/webkorps/restrouserservice/RestroUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webkorps.restrouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestroUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestroUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webkorps/restrouserservice/RestroUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webkorps.restrouserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestroUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
